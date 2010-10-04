a79813ced147cbed3caa0f359b83170f1a661f96
flash.vkv@gmail.com <flash.vkv@gmail.com@5975f468-249a-3cfa-c6af-592a3c413a5c>
2010-10-04 13:26:51 +0000

    example template

---
+++ trunk/demo_pager/tmpl_demo_pager.src.js
@@ -0,0 +1,58 @@
+
+tmpl.mailbox_pager = function(pr, _) {
+	var ns = this;
+
+	ns.node = _('div.b-mbox_pager', {css: pr.css}
+		, _((pr.next_page ? 'a' : 'span')
+			, {
+				className: 'b-mbox_pager-next',
+				title: (pr.next_page?"Перейти на следующую страницу":''),
+				href: (pr.url_mask ? rr.printx(pr.url_mask, pr.num+1) : '#/p'+(pr.num+1))
+				}
+
+			, _('span.b-mbox_pager-text', "Следующая")
+			, _('span.b-mbox_pager-key'
+				, ' Ctrl '
+				, _('span.b-mbox_pager-arr'
+					, '\u2192' //'→'
+					)
+				)
+
+			)
+
+
+		, _((pr.prev_page ? 'a' : 'span')
+			, {
+				className: 'b-mbox_pager-prev',
+				title: (pr.prev_page?"Перейти на предыдущую страницу":''),
+				href: (pr.url_mask ? rr.printx(pr.url_mask, pr.num-1) : '#/p'+(pr.num-1))
+				}
+
+			, _('span.b-mbox_pager-key'
+				, _('span.b-mbox_pager-arr'
+					, '\u2190' //'←'
+					)
+				, ' Ctrl '
+				)
+			, _('span.b-mbox_pager-text', "Следующая")
+			)
+
+
+		, _('span.b-mbox_pager-box'
+			, _.forEach(pr.pages, function(v) {
+				if (v.is_selected) {
+					return _('span.b-mbox_pager-page b-mbox_pager-page--selected', {title: "Текущая страница", text: v.num})
+					};
+
+				return _('a.b-mbox_pager-page'
+					, {
+						href: pr.url_mask ? rr.printx(pr.url_mask, v.num) : '#/p'+v.num,
+						title: rr.printx("Перейти на страницу %s", v.num),
+						text: (v.is_skip?'...':v.num)
+						}
+					);
+				})
+			)
+		)
+	};
+
