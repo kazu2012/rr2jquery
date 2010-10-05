464849a190159c09eaaf582d2d1298101746452f
flash.vkv@gmail.com <flash.vkv@gmail.com@5975f468-249a-3cfa-c6af-592a3c413a5c>
2010-10-05 12:18:40 +0000

    fix _.forEach and othe

--- trunk/demo_pager/tmpl_demo_pager.src.js
+++ trunk/demo_pager/tmpl_demo_pager.src.js
@@ -5,8 +5,8 @@ tmpl.mailbox_pager = function(pr, _) {
 	ns.node = _('div.b-mbox_pager', {css: pr.css}
 		, _((pr.next_page ? 'a' : 'span')
 			, {
-				className: 'b-mbox_pager-next',
-				title: (pr.next_page?"Перейти на следующую страницу":''),
+				css: 'b-mbox_pager-next',
+				title: (pr.next_page ? "Перейти на следующую страницу":''),
 				href: (pr.url_mask ? rr.printx(pr.url_mask, pr.num+1) : '#/p'+(pr.num+1))
 				}
 
@@ -23,8 +23,8 @@ tmpl.mailbox_pager = function(pr, _) {
 
 		, _((pr.prev_page ? 'a' : 'span')
 			, {
-				className: 'b-mbox_pager-prev',
-				title: (pr.prev_page?"Перейти на предыдущую страницу":''),
+				css: 'b-mbox_pager-prev',
+				title: (pr.prev_page ? "Перейти на предыдущую страницу":''),
 				href: (pr.url_mask ? rr.printx(pr.url_mask, pr.num-1) : '#/p'+(pr.num-1))
 				}
 
@@ -39,7 +39,7 @@ tmpl.mailbox_pager = function(pr, _) {
 
 
 		, _('span.b-mbox_pager-box'
-			, _.forEach(pr.pages, function(v) {
+			, rr.map(pr.pages, function(v) {
 				if (v.is_selected) {
 					return _('span.b-mbox_pager-page b-mbox_pager-page--selected', {title: "Текущая страница", text: v.num})
 					};
